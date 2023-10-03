1cd775d3247053d5f1f906dac7b6df987669b040
muddyrain <qiu3291002845@gmail.com>
2023-10-03 19:10:47 +0800

    feat: describe 分组

--- tools.test.js
+++ tools.test.js
@@ -1,4 +1,4 @@
-const { sum } = require('./tools.js');
+const { sum, sub, mul, div } = require('./tools.js');
 /**
  * 该文件就是一个测试文件
  * 在该文件中，我们会抒写一个一个测试用例
@@ -11,7 +11,20 @@ const { sum } = require('./tools.js');
  * param1: 测试用例的描述
  * param2: 测试用例的回调函数
  */
-test('测试加法', () => {
-  const result = sum(1, 2);
-  expect(result).toBe(3);
+
+describe('测试加减法', () => {
+  it('测试加法', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  it('测试减法', () => {
+    expect(sub(10, 5)).toBe(5);
+  });
+});
+describe('测试乘除法', () => {
+  it('测试乘法', () => {
+    expect(mul(2, 3)).toBe(6);
+  });
+  it('测试除法', () => {
+    expect(div(10, 2)).toBe(5);
+  });
 });
