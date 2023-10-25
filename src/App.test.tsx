b11a4a4140e02aa72b836a2c244ebe91886c1fff
“muddyrain” <qiu3291002845@gmail.com>
2023-10-25 15:52:06 +0800

    init

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('test-react', () => {
-  render(<App />);
-  const list = screen.getAllByTestId('list');
-  expect(list).toMatchSnapshot();
-});
